f2b19a743574364a13e5812fc058a476192bb100
lbaab <lindsey_baab@brown.edu>
2016-04-26 19:44:36 -0400

    emotion returns list

--- src/main/java/edu/brown/cs/categorizer/EmotionCategorizer.java
+++ src/main/java/edu/brown/cs/categorizer/EmotionCategorizer.java
@@ -1,6 +1,5 @@
 package edu.brown.cs.categorizer;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,27 +7,6 @@ import java.util.Map;
 
 public class EmotionCategorizer {
   Map<String, Emotion> emotions = new HashMap<String, Emotion>();
-
-  public static void main(String[] args) {
-    try {
-      EmotionDictionary dict = new EmotionDictionary(
-          "../readient/emotionDictionary.txt");
-      EmotionCategorizer ec = new EmotionCategorizer(
-          "I want to frollic around on the ground forever. Please never let go!"
-              + " I want to love you forever!",
-          dict);
-      EmotionCategorizer ec2 = new EmotionCategorizer(
-          "I hate you! leave me alone", dict);
-      EmotionCategorizer ec3 = new EmotionCategorizer(
-          "I love you. Hold me in your beautiful arms.", dict);
-      System.out.println(ec.getTopEmotion().getEmotion());
-      System.out.println(ec2.getTopEmotion().getEmotion());
-      System.out.println(ec3.getTopEmotion().getEmotion());
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-  }
 
   public EmotionCategorizer(String doc, EmotionDictionary ed) {
     String line = doc;
@@ -60,13 +38,13 @@ public class EmotionCategorizer {
     }
   }
 
-  public Emotion getTopEmotion() {
+  public List<Emotion> getTopEmotion() {
     List<Emotion> topEmotions = new LinkedList<Emotion>();
     topEmotions.addAll(emotions.values());
     topEmotions.sort(new EmotionComparator());
     if (!topEmotions.isEmpty()) {
       System.out.println(topEmotions.get(1).getEmotion());
-      return topEmotions.get(0);
+      return topEmotions;
     } else {
       return null;
     }
