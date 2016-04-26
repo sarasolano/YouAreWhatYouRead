9c02039fa04dcdaddbc40b647b39794dd5ba07cf
Natalie Tsvetkova <natalie_tsvetkova@brown.edu>
2016-04-26 16:57:26 -0400

    made it iterable over strings

--- src/main/java/edu/brown/cs/parsing/Parser.java
+++ src/main/java/edu/brown/cs/parsing/Parser.java
@@ -3,6 +3,7 @@ package edu.brown.cs.parsing;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -12,14 +13,16 @@ import org.jsoup.nodes.Document;
 import edu.stanford.nlp.ling.HasWord;
 import edu.stanford.nlp.process.DocumentPreprocessor;
 
-public class Parser implements Iterable<List<HasWord>> {
+public class Parser implements Iterable<String> {
 	String url;
 	String text;
 	DocumentPreprocessor dp;
+	List<String> sentences;
 
 	public Parser(String url) {
 		Document doc;
 		this.url = url;
+		this.sentences = new ArrayList<>();
 		try {
 			doc = Jsoup.connect(url).get();
 			String title = doc.select("h1").text();
@@ -28,15 +31,26 @@ public class Parser implements Iterable<List<HasWord>> {
 			this.text = allText;
 			Reader r = new StringReader(allText);
 			this.dp = new DocumentPreprocessor(r);
+			makeSentences();
 		} catch (IOException e) {
 			System.out.println("ERROR: Problem extracting the text.");
 		}
 	}
+	
+	private void makeSentences() {
+		for (List<HasWord> sent : dp) {
+			StringBuilder sentence = new StringBuilder();
+			for (HasWord word : sent) {
+				sentence.append(word + " ");
+			}
+			sentences.add(sentence.toString());
+		}
+	}
 
-	// example of how to use it
+//	// example of how to use it
 //	public static void main(String[] args) {
 //		Parser p = new Parser("https://en.wikipedia.org/wiki/Amazon_Reef");
-//		for (List<HasWord> sentence : p) {
+//		for (String sentence : p) {
 //			// you can use HasWords by doing .word() -> string
 //			// will fix this to use strings at some point
 //			System.out.println(sentence);
@@ -44,7 +58,7 @@ public class Parser implements Iterable<List<HasWord>> {
 //	}
 
 	@Override
-	public Iterator<List<HasWord>> iterator() {
-		return (Iterator<List<HasWord>>) dp.iterator();
+	public Iterator<String> iterator() {
+		return sentences.iterator();
 	}
 }
