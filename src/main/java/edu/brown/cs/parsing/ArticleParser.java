fa575e6d031632748f901c62c22467aa5db7acdf
Natalie Tsvetkova <natalie_tsvetkova@brown.edu>
2016-05-06 20:25:55 -0400

    added stopword/punctuation detection to the parser for the word cloud

--- src/main/java/edu/brown/cs/parsing/ArticleParser.java
+++ src/main/java/edu/brown/cs/parsing/ArticleParser.java
@@ -1,12 +1,18 @@
 package edu.brown.cs.parsing;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
@@ -21,13 +27,23 @@ public class ArticleParser implements Iterable<String> {
 	private String title;
 	private DocumentPreprocessor dp;
 	private List<String> sentences;
+	private List<String> noStopwords;
 	private WordCounter wc;
+	private static Set<String> stopwords;
 
 	public ArticleParser(String path) {
 		Document doc;
 		this.url = path;
 		this.sentences = new ArrayList<>();
+		this.noStopwords = new ArrayList<>();
 		this.wc = new WordCounter();
+		if (stopwords == null || stopwords.isEmpty()) {
+			try {
+				fillStopwords(new File("stopwords.txt"));
+			} catch (IOException e) {
+				System.out.println("ERROR: Error filling stopwords.");
+			}
+		}
 		try {
 			Response response = Jsoup.connect(path)
 			           .ignoreContentType(true)
@@ -53,11 +69,16 @@ public class ArticleParser implements Iterable<String> {
 
     for (List<HasWord> sent : dp) {
       StringBuilder sentence = new StringBuilder();
+      StringBuilder stopSentence = new StringBuilder();
       for (HasWord word : sent) {
         sentence.append(word + " ");
-        wc.increment(word.word());
+        if (!stopwords.contains(word.word().toLowerCase())) {
+        	 wc.increment(word.word().toLowerCase());
+        	 stopSentence.append(word + " ");
+        }
       }
       sentences.add(sentence.toString());
+      noStopwords.add(stopSentence.toString());
     }
   }
 
@@ -77,19 +98,36 @@ public class ArticleParser implements Iterable<String> {
 		return Collections.unmodifiableList(sentences);
 	}
 	
+	public List<String> stopSentences() {
+		return Collections.unmodifiableList(noStopwords);
+	}
+	
 	public String jsonCounts() {
 		return wc.getJSON();
+	}
+	
+	public static void fillStopwords(File stopwords) throws IOException {
+		ArticleParser.stopwords = new HashSet<>();
+		BufferedReader br = new BufferedReader(new FileReader(stopwords));
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			String word = line.trim();
+			ArticleParser.stopwords.add(word);
+		}
+		br.close();
+		List<String> punctuation = Arrays.asList(".", ",", "'", ":", "!", "?", "``", "''", "-rrb-", "-lrb-", "a", "...", "'s", "n't", "--", "'ll");
+		ArticleParser.stopwords.addAll(punctuation);
 	}
 
 	// example of how to use it
 	public static void main(String[] args) {
-		ArticleParser p = new ArticleParser("http://www.economist.com/blogs/democracyinamerica/2016/05/pivotal-primary");
+		// ArticleParser p = new ArticleParser("http://www.economist.com/blogs/democracyinamerica/2016/05/pivotal-primary");
 		// ArticleParser p = new ArticleParser("http://blogs.scientificamerican.com/cross-check/psychedelic-therapy-and-bad-trips/");
 		// ArticleParser p = new ArticleParser("http://www.nytimes.com/2016/05/04/us/politics/indiana-republican-democratic.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=span-ab-top-region&region=top-news&WT.nav=top-news");
 		// ArticleParser p = new ArticleParser("http://hotair.com/archives/2016/05/05/wow-im-not-ready-to-endorse-trump-says-paul-ryan/");
-		// ArticleParser p = new ArticleParser("http://www.helpguide.org/articles/emotional-health/anger-management.htm");
+		ArticleParser p = new ArticleParser("http://www.helpguide.org/articles/emotional-health/anger-management.htm");
 		// ArticleParser p = new ArticleParser("http://www.bustle.com/articles/158767-10-ways-to-pull-off-a-style-youre-intimidated-by");
-		for (String sentence : p) {
+		for (String sentence : p.stopSentences()) {
 			// you can use HasWords by doing .word() -> string
 			// will fix this to use strings at some point
 			System.out.println(sentence);
--- src/main/java/edu/brown/cs/parsing/WordCounter.java
+++ src/main/java/edu/brown/cs/parsing/WordCounter.java
@@ -3,20 +3,25 @@ package edu.brown.cs.parsing;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.PriorityQueue;
 import java.util.Map.Entry;
+
+import cc.mallet.util.CommandOption.Set;
 
 import com.google.gson.Gson;
 
 public class WordCounter {
-	
-	HashMap<String, Integer> wc;
-	Collection<Word> words;
-	
+
+	private HashMap<String, Integer> wc;
+	private PriorityQueue<Word> words;
+	private Collection<Word> top20;
+
 	public WordCounter() {
 		wc = new HashMap<>();
-		words = new ArrayList<>();
+		words = new PriorityQueue<>();
+		top20 = new ArrayList<>();
 	}
-	
+
 	public void increment(String word) {
 		if (wc.containsKey(word)) {
 			int value = wc.get(word);
@@ -25,22 +30,39 @@ public class WordCounter {
 			wc.put(word, 1);
 		}
 	}
-	
+
 	public String getJSON() {
 		for (Entry<String, Integer> entry : wc.entrySet()) {
 			Word word = new Word(entry.getKey(), entry.getValue());
 			words.add(word);
 		}
-		return new Gson().toJson(words);
+		for (int i = 0; i < 19; i++) {
+			Word w = words.poll();
+			if (w != null) {
+				top20.add(w);
+			}
+		}
+		return new Gson().toJson(top20);
 	}
-	
-	private class Word {
+
+	private class Word implements Comparable<Word> {
 		String text;
-		int weight; 
-		
+		int weight;
+
 		private Word(String text, int weight) {
 			this.text = text;
 			this.weight = weight;
+		}
+
+		@Override
+		public int compareTo(Word o) {
+			if (this.weight < o.weight) {
+				return 1;
+			} else if (this.weight == o.weight) {
+				return 0;
+			} else {
+				return -1;
+			}
 		}
 	}
 
